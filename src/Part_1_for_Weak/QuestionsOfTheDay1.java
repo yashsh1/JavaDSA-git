544031de0eb12ad8b2ad33be865c3580c25423d1
yash sharma <yashnavalsharma4878@gmail.com>
2026-01-13 12:15:49 +0530

    Initial commit

---
+++ src/Part_1_for_Weak/QuestionsOfTheDay1.java
@@ -0,0 +1,10 @@
+package Part_1_for_Weak;
+
+public class QuestionsOfTheDay1 {
+    public static void main(String[] args ){
+// ################################################### Easy #########################################
+
+//        question 1  leetcode :-
+
+    }
+}
