42e321309865b776fe15f2c6af373a0b7df77d15
yash sharma <yashnavalsharma4878@gmail.com>
2026-01-13 12:33:39 +0530

    Array start for 1 weak

--- src/Part_1_for_Weak/QuestionsOfTheDay1.java
+++ src/Part_1_for_Weak/QuestionsOfTheDay1.java
@@ -2,7 +2,7 @@ package Part_1_for_Weak;
 
 public class QuestionsOfTheDay1 {
     public static void main(String[] args ){
-// ################################################### Easy #########################################
+// #################################################### Easy #########################################
 
 //        question 1  leetcode :-
 
