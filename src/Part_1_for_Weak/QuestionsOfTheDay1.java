6433db2b47d34f01fd14abd2cca4deff2892c5e7
yash sharma <yashnavalsharma4878@gmail.com>
2026-01-17 21:30:35 +0530

    Array start for 1 weak

--- src/Part_1_for_Weak/QuestionsOfTheDay1.java
+++ src/Part_1_for_Weak/QuestionsOfTheDay1.java
@@ -103,13 +103,36 @@ public class QuestionsOfTheDay1 {
         return sum2 - sum;
     }
 
+//    question 3 leetcode 27:-
+     /*
+     Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+      Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+        Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+        Return k.
+      */
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
     // MAIN METHOD (for testing only)
     public static void main(String[] args) {
         int[] nums = {3, 0, 1};
         int[] nums1 = {1, 1, 0, 1, 1, 1};
         int[] nums2 = {12, 345, 2, 6, 7896};
+        int[] nums3= {3,2,2,3};
+        int val=3;
         System.out.println(missingNumber(nums)); // Output: 2
         System.out.println(QuestionsOfTheDay1.findMaxConsecutiveOnes(nums1)); // Output: 3
         System.out.println(QuestionsOfTheDay1.findNumbers(nums2)); // Output: 2
+        System.out.println(QuestionsOfTheDay1.removeElement(nums3,val)); // Output: 2
     }
 }
