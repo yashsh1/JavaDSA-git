9d76859d017f6491d0d33e453540fb92838fa06a
yash sharma <yashnavalsharma4878@gmail.com>
2026-01-17 21:31:21 +0530

    Array start for 1 weak

--- src/Part_1_for_Weak/QuestionsOfTheDay1.java
+++ src/Part_1_for_Weak/QuestionsOfTheDay1.java
@@ -113,7 +113,6 @@ public class QuestionsOfTheDay1 {
 
     public static int removeElement(int[] nums, int val) {
         int k = 0;
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
                 nums[k] = nums[i];
