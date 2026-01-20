20dc94659f9c906748ce5f3c3ac2c7068c739d20
yash sharma <yashnavalsharma4878@gmail.com>
2026-01-20 21:53:41 +0530

    Array start for 1 weak

--- src/Part_1_for_Weak/QuestionsOfTheDay1.java
+++ src/Part_1_for_Weak/QuestionsOfTheDay1.java
@@ -124,7 +124,6 @@ public class QuestionsOfTheDay1 {
 
     // MAIN METHOD (for testing only)
     public static void main(String[] args) {
-        
         int[] nums = {3, 0, 1};
         int[] nums1 = {1, 1, 0, 1, 1, 1};
         int[] nums2 = {12, 345, 2, 6, 7896};
