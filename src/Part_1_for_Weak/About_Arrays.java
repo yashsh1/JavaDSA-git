80d3166f567a5bbe5c837a7c72d5669d5ae6214f
yash sharma <yashnavalsharma4878@gmail.com>
2026-02-11 10:48:07 +0530

    Array start for 1 weak

--- src/Part_1_for_Weak/About_Arrays.java
+++ src/Part_1_for_Weak/About_Arrays.java
@@ -16,7 +16,7 @@ public class About_Arrays {
         this.releaseYear = releaseYear;
         this.director = director;
     }
-
+    @Override
     public String toString() {
         return this.name + ", directed by " + this.director + ", released in " + this.releaseYear;
     }
@@ -38,16 +38,31 @@ public class About_Arrays {
 //            System.out.println();
 //            dvdCollection[i]=new About_Arrays(name,year,director);
 //        }
+//                                or
+//        dvdCollection[0] = new About_Arrays("The Shawshank Redemption", 1994, "Frank Darabont");
+//        dvdCollection[1] = new About_Arrays("The Dark Knight", 2008, "Christopher Nolan");
+//        dvdCollection[2] = new About_Arrays("Inception", 2010, "Christopher Nolan");
+//        dvdCollection[3] = new About_Arrays("The Matrix", 1999, "Lana Wachowski, Lilly Wachowski");
+//        dvdCollection[4] = new About_Arrays("Forrest Gump", 1994, "Robert Zemeckis");
+//                                or
 
-        dvdCollection[0] = new About_Arrays("The Shawshank Redemption", 1994, "Frank Darabont");
-        dvdCollection[1] = new About_Arrays("The Dark Knight", 2008, "Christopher Nolan");
-        dvdCollection[2] = new About_Arrays("Inception", 2010, "Christopher Nolan");
-        dvdCollection[3] = new About_Arrays("The Matrix", 1999, "Lana Wachowski, Lilly Wachowski");
-        dvdCollection[4] = new About_Arrays("Forrest Gump", 1994, "Robert Zemeckis");
+        About_Arrays incrediblesDVD = new About_Arrays("The Incredibles", 2004, "Brad Bird");
+        About_Arrays findingDoryDVD = new About_Arrays("Finding Dory", 2016, "Andrew Stanton");
+        About_Arrays lionKingDVD = new About_Arrays("The Lion King", 2019, "Jon Favreau");
+        About_Arrays avengersDVD = new About_Arrays("The Avengers", 2012, "Joss Whedon");
+        About_Arrays theShawshank= new About_Arrays("The Shawshank Redemption", 1994, "Frank Darabont");
+
+        dvdCollection[0] = incrediblesDVD;
+        dvdCollection[1] = findingDoryDVD;
+        dvdCollection[2] = lionKingDVD;
+        dvdCollection[3] = avengersDVD;
+        dvdCollection[4] = theShawshank;
+
 
         for (var dvd: dvdCollection){
-            System.out.println(dvd.toString());
+            System.out.println(dvd);
         }
+        System.out.println(dvdCollection);
 
 
         scanner.close();
