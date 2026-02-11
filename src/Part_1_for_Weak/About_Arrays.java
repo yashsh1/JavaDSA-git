9e0c7b29b9816f5272d5fc0513e666b274b0fdf1
yash sharma <yashnavalsharma4878@gmail.com>
2026-02-12 04:47:25 +0530

    Array start for 1 weak

--- src/Part_1_for_Weak/About_Arrays.java
+++ src/Part_1_for_Weak/About_Arrays.java
@@ -39,8 +39,6 @@ public class About_Arrays {
 //            dvdCollection[i]=new About_Arrays(name,year,director);
 //        }
 //                                or
-
-        
 //        dvdCollection[0] = new About_Arrays("The Shawshank Redemption", 1994, "Frank Darabont");
 //        dvdCollection[1] = new About_Arrays("The Dark Knight", 2008, "Christopher Nolan");
 //        dvdCollection[2] = new About_Arrays("Inception", 2010, "Christopher Nolan");
